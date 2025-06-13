0758651896cbe36bf5651fb4cae703b39128f488
Java-S12138 <java_s@qq.com>
2025-06-13 17:18:21 +0800

    3.15.1505

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -6,6 +6,7 @@ use lcu::{
 };
 use tauri::Emitter;
 use tauri::{Listener, Manager};
+use tauri_plugin_window_state::{StateFlags};
 
 #[tokio::main]
 pub async fn run() {
@@ -33,7 +34,14 @@ pub async fn run() {
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_single_instance::init(|app, argv, cwd| {
+            // 当尝试启动新实例时，聚焦主窗口
+            let _ = app.get_webview_window("mainWindow")
+                .expect("no main window")
+                .set_focus();
+        }))
         .plugin(tauri_plugin_window_state::Builder::default()
+            .with_state_flags(StateFlags::POSITION)
             .with_denylist(&["background","queryMatchWindow","matchAnalysisWindow","recentMatchWindow"]).build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
